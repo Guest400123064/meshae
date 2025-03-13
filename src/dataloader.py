ec7f0faa0c6d49a9ad4a0fa8be931889cff6fbc6
guest400123064 <lgfz1003@icloud.com>
2025-03-13 03:01:02 -0400

    obj loader

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,18 +1,93 @@
+from pathlib import Path
+from typing import Any
+
+import torch
+from ezgatr.interfaces import point
 from pytorch3d.io import load_obj
+from pytorch3d.ops import sample_points_from_meshes
 from pytorch3d.structures import Meshes
 
 
-def load_normalized_mesh_from_obj(path, normalize=True) -> Meshes:
-    r"""Load and normalize a mesh.
+class Pix3DObject:
+    r"""Helper class for creating point cloud samples.
 
-    Load a ``pytorch3d.structures.Meshes`` object from a given
-    path. The loaded mesh is centered around the mean and scaled
-    to ``(-1, 1)``.
+    Parameters
+    ----------
+    root: pathlib.Path
+        Path to the root folder of the raw Pix3D dataset.
+    meta: dict[str, Any]
+        Metadata from ``pix3d.json``.
+    load_on_init: bool
+        Whether load the object with default settings on initialization.
+        Please refer to ``Pix3DObject.load_obj()`` method for the default
+        settings.
     """
-    v, f, _ = load_obj(path)
-    if normalize:
-        c = v.mean(dim=0)
-        s = max(v.abs().max(dim=0).values)
-        v = (v - c) / s
 
-    return Meshes(verts=[v], faces=[f.verts_idx])
+    def __init__(
+        self, root: Path, meta: dict[str, Any], load_on_init: bool = True,
+    ) -> None:
+        self.root = root
+        self.meta = meta
+        self.mesh = None
+        if load_on_init:
+            self.load_obj()
+
+    @property
+    def is_initialized(self):
+        return self.mesh is not None
+
+    def sample_points(
+        self, num_samples: int = 1024, to_pga: bool = True,
+    ) -> torch.Tensor:
+        r"""Sample a point cloud with size ``num_samples``.
+
+        The sampled point cloud is returned in a batch with a single record.
+        If ``to_pga`` is set to ``True``, a channel dimension with size 1 is
+        automatically ``unsqueeze``-ed to the second to the last dimension.
+
+        Parameters
+        ----------
+        num_samples: int
+            Number of points in the returned point cloud.
+        to_pga: bool
+            If returning point cloud encoded as PGA multi-vectors.
+
+        Returns
+        -------
+        torch.Tensor
+            Sampled point cloud in 3D or PGA multi-vectors. Returning tensor
+            has shape ``(B, N, 3)`` or ``(B, N, 1, 16)``.
+        """
+        if not self.is_initialized:
+            raise RuntimeError("Call ``Pix3DObject.load_obj`` to load mesh first.")
+
+        ret = sample_points_from_meshes(self.mesh, num_samples)
+        if to_pga:
+            return point.encode(ret).unsqueeze(-2)
+        return ret
+
+    def load_obj(self, normalize: bool = True) -> "Pix3DObject":
+        r"""Load the object into mesh and assign to ``self.mesh``.
+
+        Load a ``pytorch3d.structures.Meshes`` object from a given
+        path. The loaded .
+
+        Parameters
+        ----------
+        normalize: bool
+            Loaded mesh is centered around the mean and scaled to
+            ``(-1, 1)`` if set to ``True``.
+
+        Returns
+        -------
+        Pix3DObject
+            Returning a reference to self.
+        """
+        verts, faces, _ = load_obj(self.root / self.meta["model"])
+        if normalize:
+            center = verts.mean(dim=0)
+            scaler = max(verts.abs().max(dim=0).values)
+            verts = (verts - center) / scaler
+
+        self.mesh = Meshes(verts=[verts], faces=[faces.verts_idx])
+        return self
