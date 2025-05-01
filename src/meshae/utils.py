1fbf367cb3fb363fada98d465051958363ecd06a
guest400123064 <lgfz1003@icloud.com>
2025-04-30 23:06:32 -0400

    symbol rename

--- src/meshae/utils.py
+++ src/meshae/utils.py
@@ -4,34 +4,34 @@ from torchtyping import TensorType
 
 
 def quantize(
-    t: TensorType[..., float],
+    tensor: TensorType[..., float],
     *,
     high_low: tuple[float, float],
     num_bins: int,
 ) -> TensorType[..., int]:
     r"""Discretize continuous inputs into bin IDs."""
 
-    h, l = high_low
-    if h <= l:
+    high, low = high_low
+    if high <= low:
         msg = f"High must be strictly greater than low, got: <{high_low}>."
         raise ValueError(msg)
 
-    t = ((t - l) / (h - l)) * num_bins - 0.5
-    return t.round().long().clamp(min=0, max=(num_bins - 1))
+    tensor = ((tensor - low) / (high - low)) * num_bins - 0.5
+    return tensor.round().long().clamp(min=0, max=(num_bins - 1))
 
 
 def dequantize(
-    t: TensorType[..., int],
+    tensor: TensorType[..., int],
     *,
     high_low: tuple[float, float],
     num_bins: int,
 ) -> TensorType[..., float]:
     r"""Reconstruct continuous inputs from discrete bin IDs."""
 
-    h, l = high_low
-    if h <= l:
+    high, low = high_low
+    if high <= low:
         msg = f"High must be strictly greater than low, got: <{high_low}>."
         raise ValueError(msg)
 
-    t = (t.float() + 0.5) / num_bins
-    return t * (h - l) + l
+    tensor = (tensor.float() + 0.5) / num_bins
+    return tensor * (high - low) + low
