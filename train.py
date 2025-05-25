af3cdf65b358e381b90cb3fc7125ffb0e2f30434
guest400123064 <lgfz1003@icloud.com>
2025-05-24 22:32:40 -0400

    basic training cli

--- train.py
+++ train.py
@@ -1,54 +1,74 @@
 from __future__ import annotations
 
 import argparse
+import yaml
+from typing import Any
 
-import numpy as np
 import torch
-
-from meshae import MeshAEFeatEmbedConfig, MeshAEModel
+from easydict import EasyDict
+from meshae import MeshAEModel, MeshAEModelConfig
 from meshae.dataset import MeshAEDataset, MeshAECollateFn
 from meshae.trainer import MeshAETrainer
 
 
 def get_args() -> argparse.Namespace:
-    pass
+    parser = argparse.ArgumentParser(
+        prog="train.py",
+        description="Train mesh VQ-VAE.",
+        epilog="Please raise an Issue for questions.",
+    )
+    parser.add_argument(
+        "--config",
+        type=str,
+        required=True,
+        help="Path to the model and trainer config.",
+    )
+    parser.add_argument(
+        "--data-train",
+        type=str,
+        required=True,
+        help="Path to the directory containing training mesh objects.",
+    )
+    parser.add_argument(
+        "--data-valid",
+        type=str,
+        required=False,
+        default=None,
+        help="Path to the directory containing validation mesh objects.",
+    )
+    return parser.parse_args()
 
 
-def main(args):
-    collate_fn = MeshAECollateFn()
+def create_trainer(config: EasyDict[str, Any]) -> MeshAETrainer:
 
-    ds_train = MeshAEDataset("data/objaverse/train/")
-    ds_valid = MeshAEDataset("data/objaverse/valid/")
+    model = MeshAEModel(**MeshAEModelConfig(**config.model).asdict())
+    optimizer = torch.optim.AdamW(model.parameters(), **config.optimizer)
 
     trainer = MeshAETrainer(
-        model=model, loss_func=None, optimizer=optimizer,
+        model,
+        loss_func=None,
+        optimizer=optimizer,
     )
+    return trainer
+
+
+def main() -> None:
+    args = get_args()
+    with open(args.config) as f:
+        config = EasyDict(**yaml.safe_load(f))
+
+    training_dataset = MeshAEDataset(args.data_train, sort_by=config.dataset.sort_by)
+    eval_dataset = args.data_valid
+    if isinstance(eval_dataset, str):
+        eval_dataset = MeshAEDataset(eval_dataset, sort_by=config.dataset.sort_by)
+
+    trainer = create_trainer(config)
     trainer.train(
-        train_dataset=ds_train,
-        eval_dataset=ds_valid,
-        num_epochs=2,
-        per_device_batch_size=8,
-        collate_fn=collate_fn,
+        train_dataset=training_dataset,
+        eval_dataset=eval_dataset,
+        collate_fn=MeshAECollateFn(**config.collate_fn),
+        **config.train,
     )
-
-
-feat_configs = {
-    "vrtx": MeshAEFeatEmbedConfig(high_low=(0.5, -0.5)),
-    "acos": MeshAEFeatEmbedConfig(high_low=(np.pi, 0.0)),
-    "norm": MeshAEFeatEmbedConfig(high_low=(1.0, -1.0), num_bins=512),
-    "area": MeshAEFeatEmbedConfig(high_low=(1.0, 0.0)),
-}
-model = MeshAEModel(
-    feat_configs,
-    num_sageconv_layers=1,
-    num_quantizers=2,
-    num_encoder_layers=2,
-    num_decoder_layers=2,
-    num_refiner_layers=2,
-    bin_smooth_blur_sigma=0.0,
-).to("cuda:0")
-
-optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=1e-6)
 
 
 if __name__ == "__main__":
